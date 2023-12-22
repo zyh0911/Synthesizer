c508ff50aff4951e17aa4ccbf4581e0f9e1fa761
zyh0911 <1815451667@qq.com>
2023-12-22 23:03:38 +0800

    add some change

--- result/result4.v
+++ result/result4.v
@@ -5,9 +5,7 @@ module \./case/newcase4  (
     po0  );
   input  pi0, pi1;
   output po0;
-  wire new_n4;
-  NAND2 g0(.a(pi1), .b(pi0), .O(new_n4));
-  INV1  g1(.a(new_n4), .O(po0));
+  NAND2 g0(.a(pi1), .b(pi0), .O(po0));
 endmodule
 
 
